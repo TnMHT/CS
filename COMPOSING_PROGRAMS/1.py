5ecd0f168227d7b4c490932e2c3713f9046962e0
TeonMehta <teonmehta@gmail.com>
2022-04-22 16:36:58 -0400

    completed 1.3 & 1.4

---
+++ COMPOSING_PROGRAMS/1.py
@@ -0,0 +1,31 @@
+#SECTION 1
+
+
+from audioop import add, mul
+
+
+#FUNCTIONS
+
+def square(x):
+    mul(x,x)
+
+def sum_squares(x, y):
+    return add(square(x), square(y))
+
+# The domain of a function is the set of arguments it can take.
+# The range of a function is the set of values it can return. 
+# The intent of a function is the relationship it computes between inputs and output (as well as any side effects it might generate). 
+
+# Each function should have one job
+
+
+def pressure(v, t, n=6.022e23):
+        """Compute the pressure in pascals of an ideal gas.
+
+        v -- volume of gas, in cubic meters
+        t -- absolute temperature in degrees kelvin
+        n -- particles of gas (default: one mole)
+        """
+        k = 1.38e-23  # Boltzmann's constant
+        return n * k * t / v
+
