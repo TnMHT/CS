1b05f71497d22a8ec90e2d0118811b3766ecc87b
TeonMehta <teonmehta@gmail.com>
2022-05-04 20:51:01 -0400

    reversed string exercise

---
+++ DSA/ARRAYS/reverseString.js
@@ -0,0 +1,10 @@
+
+function reverse(string){
+    let reversedString = ''
+    for(let i = string.length - 1; i >=0; i--){
+        reversedString += string[i]
+    }
+    console.log(reversedString)
+}
+
+reverse('abcdef')
