5dd58ab7c9de7207df09c85db32efe01290de4a7
TeonMehta <teonmehta@gmail.com>
2022-06-01 16:46:54 -0400

    Added notes about 1.7 recursion completed

--- COMPOSING_PROGRAMS/1.py
+++ COMPOSING_PROGRAMS/1.py
@@ -82,3 +82,6 @@ def fib_test():
 # A function is called recursive if the body of the function calls the function itself, either directly or indirectly. 
 
 
+# Recursive functions generally have a base case or multiply base cases
+# Recursive calls always have a certain character: they simplify the original problem. 
+# Recursive functions express computation by simplifying problems incrementally
