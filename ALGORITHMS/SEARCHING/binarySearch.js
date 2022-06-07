83ef229e074edf0ca263a4bb8f044f0e78601606
TeonMehta <teonmehta@gmail.com>
2022-06-07 16:17:42 -0400

    completed searching section

---
+++ ALGORITHMS/SEARCHING/binarySearch.js
@@ -0,0 +1,8 @@
+
+///BINARY SEARCH
+// If data is sorted we can discard half the items at a time
+// Good for Trees
+
+const numbers = [2, 44, 55, 7, 22, 3, 45, 19, 78, 0];
+
+
---
+++ ALGORITHMS/SEARCHING/searching.js
@@ -0,0 +1,31 @@
+/*
+
+SEARCHING
+
+LINEAR SEARCH
+- finding a target value within a list using traversal 
+- 
+
+
+BINARY SEARCH
+
+DEPTH FIRST SEARCH
+BREADTH FIRST SEARCH
+ */
+
+// LINEAR SEARCH
+// finding a target value within a list using traversal
+// worst case is 0(n)
+
+const names = ["tom", "jerry", "dre", "james", "karen", "lisa"];
+
+names.indexOf("lisa");
+
+names.findIndex((item) => item === "james");
+
+names.find((item) => item === "jerry");
+
+names.includes('tom')
+
+
+// BINARY SEARCH
